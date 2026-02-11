950ed685fc2bcdde0f4e9b0bb76d55d90350f898
sommargron <karlemritz@gmail.com>
2026-02-11 18:25:11 +0000

    a2

--- 03_main.R
+++ 03_main.R
@@ -16,7 +16,7 @@ expo <- c("vapo",
           "vgdf", 
           "vgdffm")
 
-# 03_00 LONGITUDINAL ANALYSES --------------------------------------------------
+# 03_00 LONGITUDINAL ANALYSES OF INCIDENT --------------------------------------
 opeck_a1 <- left_join(
   opeck_o2,
   opeck_e3,
@@ -171,3 +171,159 @@ p03_00 <- opeck_res_a1 %>%
     legend.position = 'bottom'
   )
 ggsave(filename = 'opeck/outputs/plots/p03_00.svg', p03_00, width = 10, height = 6)
+
+# 03_00 LONGITUDINAL ANALYSES OF INCIDENT --------------------------------------
+opeck_a2 <- left_join(
+  opeck_o2,
+  opeck_e3,
+  by = 'opeck_id'
+) %>%  
+  filter(n18 < date_enrol,
+         is.na(n07)
+  ) %>% 
+  mutate(
+    time = pmin(
+      date_death,
+      date_lofup,
+      eskd,
+      as.Date.character('2023-03-31', format = '%Y-%m-%d'),
+      na.rm = T) - date_enrol,
+    event = if_else(time != eskd - date_enrol | is.na(eskd), 0, 1),
+    time = as.integer(time),
+    year_enrol = year(date_enrol)
+  )
+
+surv_a2 <- Surv(
+  time = opeck_a2$time,
+  event = opeck_a2$event
+)
+
+fit_a2_m0 <- map(
+  expo,
+  \(x) coxph(
+    as.formula(paste0('surv_a2 ~ ', 
+                      x)),
+    data = opeck_a2
+  )
+)
+
+fit_a2_m1 <- map(
+  expo,
+  \(x) coxph(
+    as.formula(paste0('surv_a2 ~ ', 
+                      x, 
+                      ' + rcs(age, 3) + sex + year_enrol')),
+    data = opeck_a2
+  )
+)
+
+fit_a2_m2 <- map(
+  expo,
+  \(x) coxph(
+    as.formula(paste0('surv_a2 ~ ', 
+                      x, 
+                      ' + rcs(age, 3) + sex + year_enrol',
+                      ' + smo + rcs(bmi,3) + alc')),
+    data = opeck_a2
+  )
+)
+
+fit_a2_m3 <- map(
+  expo,
+  \(x) coxph(
+    as.formula(paste0('surv_a2 ~ ', 
+                      x, 
+                      ' + rcs(age, 3) + sex + year_enrol',
+                      ' + smo + rcs(bmi,3) + alc',
+                      ' + rcs(dep,3) + eth + rcs(qua,3) + inc')),
+    data = opeck_a2
+  )
+)
+
+fit_a2_m4 <- map(
+  expo,
+  \(x) coxph(
+    as.formula(paste0('surv_a2 ~ ', 
+                      x, 
+                      ' + rcs(age, 3) + sex + year_enrol',
+                      ' + smo + rcs(bmi,3) + alc',
+                      ' + rcs(dep,3) + eth + rcs(qua,3) + inc',
+                      ' + rcs(ldl,3) + rcs(gly,3) + dia + hpt')),
+    data = opeck_a2
+  )
+)
+
+opeck_res_a2 <- 
+  map2(
+    list(
+      map(fit_a2_m0, broom::tidy) %>% bind_rows,
+      map(fit_a2_m1, broom::tidy) %>% bind_rows,
+      map(fit_a2_m2, broom::tidy) %>% bind_rows,
+      map(fit_a2_m3, broom::tidy) %>% bind_rows,
+      map(fit_a2_m4, broom::tidy) %>% bind_rows
+    ),
+    0:4,
+    \(x, y) bind_rows(x) %>% 
+      mutate(model = y) %>% 
+      filter(term %in% expo)
+  ) %>% 
+  bind_rows() %>% 
+  mutate(term_f = case_match(term,
+                             'asth' ~ 'Asthmagens',
+                             'dies' ~ 'Diesel exhaust',
+                             'dust' ~ 'Dusts',
+                             'dust_bio' ~ 'Biological dusts',
+                             'dust_min' ~ 'Mineral dusts',
+                             'fibr' ~ 'Fibres',
+                             'fume' ~ 'Fumes',
+                             'gas' ~ 'Gasses',
+                             'gasf' ~ 'Gasses and fumes',
+                             'meta' ~ 'Metals',
+                             'mist' ~ 'Mists',
+                             'vapo' ~ 'Vapours',
+                             'vgdf' ~ 'VGDF',
+                             'vgdffm' ~ 'VGDFFM'))
+
+p03_01 <- opeck_res_a2 %>%  
+  filter(!term %in% c('asth', 'gasf'),
+         model != 0) %>% 
+  mutate(
+    model = factor(model) %>% fct_rev(),
+    term  = factor(term) %>% fct_rev(),
+    term_id = as.numeric(term)
+  ) %>% 
+  ggplot(aes(y = term,
+             group = model,
+             x = exp(estimate), 
+             xmin = exp(estimate - 1.96*std.error), 
+             xmax = exp(estimate + 1.96*std.error),
+             colour = model)) +
+  geom_rect(
+    aes(
+      ymin = term_id - 0.44,
+      ymax = term_id + 0.44,
+      xmin = 0.95,
+      xmax = 1.22
+    ),
+    fill = 'lightgrey',
+    inherit.aes = FALSE,
+    alpha = 0.05
+  ) +
+  geom_text(aes(y = term, x = 0.9475, label = term_f), hjust = 1, colour = 'black') +
+  geom_vline(aes(xintercept = 1), colour = 'white', size = 2) +
+  geom_point(aes(shape = model), position = position_dodge(.8)) +
+  geom_errorbar(orientation = 'y', position = position_dodge(.8), width = .5) +
+  scale_x_continuous(trans = 'log', 
+                     breaks = c(.95, 1, 1.05, 1.1, 1.15, 1.2),
+                     expand = c(0,0)) +
+  scale_colour_grey(start = .1, end = .7, breaks = 0:4) + 
+  scale_shape_manual(values = 15:19, breaks = 0:4) +
+  coord_cartesian(xlim = c(.7, 1.51)) +
+  labs(shape = 'Model', colour = 'Model', x = 'Adjusted HR (95% CI) for incident CKD per 10 EU-year') +
+  theme_void() +
+  theme(
+    axis.text.x = element_text(size = 10, vjust = 1),
+    axis.title.x = element_text(size = 12, hjust = 1, vjust = 0),
+    legend.position = 'bottom'
+  )
+ggsave(filename = 'opeck/outputs/plots/p03_01.svg', p03_01, width = 10, height = 6)
